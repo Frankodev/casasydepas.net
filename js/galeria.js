281ea8a8e247a2f11fef1bb02f8be73cf1f471ca
Francisco <xavicodev@gmail.com>
2021-11-15 12:52:08 -0600

    cambios en ui/ux diamante

--- js/galeria.js
+++ js/galeria.js
@@ -1,25 +1,20 @@
 /** @format */
 
-// variable del div contenedor de la foto principal
-const imgPrincipal = document.querySelector("#img_principal");
-
-// variable de las fotos de la galeria
+// declarando variables
 const imagenes = document.querySelectorAll("#galeria img");
-
-// variable de la imagen activa
-const imagenActiva = document.querySelector("#img-activa");
-
-// Indice de la imagen a la que hacemos click
-let indiceImagen = 0;
+const imagenActiva = document.querySelector("#img_activa");
 
 // Tareas:
-// 1.- al dar click en una foto de la galeria esta debera de aparecer en la 'foto principal gde'
+// 1.- al dar click en una foto de la galeria esta debera de aparecer en la img principal 'id=img-activa'
 
+// función que cambia la imagen activa 'Imagen Principal'
 const cambiarImagen = (event) => {
   imagenActiva.src = event.target.src;
+  // indiceImagen utiliza el metodo Array.from('element').indexOf('target'), para saber el indice del objeto dentro del array, se obtiene al ejecutar la función evento.click()
   indiceImagen = Array.from(imagenes).indexOf(event.target);
+  // console.log(indiceImagen);
 };
-
+// metodo de Array 'forEach' para rrecorrer el array de imagenes y obtener su indice en evento 'click', wl evento también llama a la función 'cambiarImagne'
 imagenes.forEach((imagen) => {
   imagen.addEventListener("click", cambiarImagen);
 });
