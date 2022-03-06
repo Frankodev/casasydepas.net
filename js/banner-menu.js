0ecd325b75383f95652b60b1aeb6405a0d82f068
Francisco <xavicodev@gmail.com>
2022-03-06 15:49:24 -0600

    En este commit se hicieron cambios grandes a toda la maquetación de la aplicación, se incorporan las carpetas de INMOBILIARIAS y MARKETPLACE, donde se cargaran los portafolios de las inmobiliarias 1 por 1 y donde se cargaran los productos y servicios que paguen la publicidad para estar dentro de la aplicación web - movil, se procede con el despliegue a producción y así mismo se comenzara a hacer marketing y publicidad para que las inmobiliarias nos contraten un portafolio y las tiendas y servicios comiencen a publicarse con casasydepas.net.

---
+++ js/banner-menu.js
@@ -0,0 +1,59 @@
+/** @format */
+
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+let darAltaInmobiliaria = document.querySelector(".activ");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+darAltaInmobiliaria.onclick = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
+
+// Clase que crea el loader
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 50,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
